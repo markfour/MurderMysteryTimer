eeb5e8ef02d4b6bb625c271f45855218c64c9a46
markfour <axhfaro@gmail.com>
2022-12-19 02:42:15 +0900

    Initialize

---
+++ MurderMysteryTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MurderMysteryTimer
+//
+//  Created by kazumi hayashida on 2022/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
