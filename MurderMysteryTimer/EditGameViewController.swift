8d1662caf7833c9b9cf75460c6580e49f7d51c43
markfour <axhfaro@gmail.com>
2022-12-26 23:44:00 +0900

    Add EditGameViewController

---
+++ MurderMysteryTimer/EditGameViewController.swift
@@ -0,0 +1,12 @@
+//
+//  EditGameViewController.swift
+//  MurderMysteryTimer
+//
+//  Created by Kazumi Hayashida on 2022/12/26.
+//
+
+import UIKit
+
+final class EditGameViewController: UIViewController {
+
+}
