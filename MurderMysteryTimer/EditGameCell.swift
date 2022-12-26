82897e7933dd180d03a43be3e28d28af989d4d95
markfour <axhfaro@gmail.com>
2022-12-27 00:52:39 +0900

    Add EditGameCell

---
+++ MurderMysteryTimer/EditGameCell.swift
@@ -0,0 +1,22 @@
+//
+//  EditGameCell.swift
+//  MurderMysteryTimer
+//
+//  Created by Kazumi Hayashida on 2022/12/27.
+//
+
+import UIKit
+
+final class EditGameCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
