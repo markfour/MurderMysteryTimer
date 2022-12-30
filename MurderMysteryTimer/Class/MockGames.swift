8ebb1f7bae713d9a4a7db43706597a8e2f9b24a2
markfour <axhfaro@gmail.com>
2022-12-30 23:41:50 +0900

    Implement MockGames

--- MurderMysteryTimer/Class/MockGames.swift
+++ MurderMysteryTimer/Class/MockGames.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
-final class mockGames {
+final class MockGames {
     static func games() -> [Game] {
-
+        var games = [Game]()
+        games.append(Game(id: 0, title: "犯人はヤスー", phase: []))
+        games.append(Game(id: 1, title: "別荘殺人事件", phase: []))
+        games.append(Game(id: 2, title: "港の事件", phase: []))
+        games.append(Game(id: 3, title: "屋敷の主人が死んだ", phase: []))
+        return games
     }
 }
