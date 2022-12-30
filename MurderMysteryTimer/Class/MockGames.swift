a14df30090f6fcb817632cccd93048de0996c2ca
markfour <axhfaro@gmail.com>
2022-12-30 22:50:42 +0900

    Add mockGames

---
+++ MurderMysteryTimer/Class/MockGames.swift
@@ -0,0 +1,14 @@
+//
+//  MockGames.swift
+//  MurderMysteryTimer
+//
+//  Created by kazumi hayashida on 2022/12/30.
+//
+
+import Foundation
+
+final class mockGames {
+    static func games() -> [Game] {
+
+    }
+}
