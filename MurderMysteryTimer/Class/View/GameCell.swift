a44509d965f4e8bce376e4f9e0d56fb21e69bf99
markfour <axhfaro@gmail.com>
2023-01-07 22:09:15 +0900

    Add GameCell

---
+++ MurderMysteryTimer/Class/View/GameCell.swift
@@ -0,0 +1,23 @@
+//
+//  GameCell.swift
+//  MurderMysteryTimer
+//
+//  Created by kazumi hayashida on 2023/01/07.
+//
+
+import UIKit
+
+class GameCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
