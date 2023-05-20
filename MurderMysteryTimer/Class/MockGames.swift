1b7cc69e326c3c940eb7b5198dc25ccabf0782fe
markfour <axhfaro@gmail.com>
2023-05-20 23:55:53 +0900

    Add GamePhase

--- MurderMysteryTimer/Class/MockGames.swift
+++ MurderMysteryTimer/Class/MockGames.swift
@@ -10,11 +10,17 @@ import Foundation
 final class MockGames {
     static func games() -> [Game] {
         var games = [Game]()
-        let phasees = [GamePhase]()
-        games.append(Game(id: 0, title: "犯人はヤスー", phases: []))
-        games.append(Game(id: 1, title: "別荘殺人事件", phases: []))
-        games.append(Game(id: 2, title: "港の事件", phases: []))
-        games.append(Game(id: 3, title: "屋敷の主人が死んだ", phases: []))
+        let phasees:[GamePhase] = [
+            GamePhase(title: "オープニング", interval: 360),
+            GamePhase(title: "話し合い1", interval: 360),
+            GamePhase(title: "話し合い2", interval: 360),
+            GamePhase(title: "犯人投票", interval: 360),
+            GamePhase(title: "エンディング", interval: 360)
+        ]
+        games.append(Game(id: 0, title: "犯人はヤスー", phases: phasees))
+        games.append(Game(id: 1, title: "別荘殺人事件", phases: phasees))
+        games.append(Game(id: 2, title: "港の事件", phases: phasees))
+        games.append(Game(id: 3, title: "屋敷の主人が死んだ", phases: phasees))
         return games
     }
 }
