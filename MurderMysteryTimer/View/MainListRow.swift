55168801c3e697ec1c4ae083f7dc297cc4613b39
Kazumi Hayashida <axhfaro@gmail.com>
2025-12-10 00:09:27 +0900

    フェーズ終了時に、ボタンと時間を非表示にした

--- MurderMysteryTimer/View/MainListRow.swift
+++ MurderMysteryTimer/View/MainListRow.swift
@@ -14,8 +14,10 @@ struct MainListRow: View {
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
-                Text(phaseTimer.formattedTime)
-                    .font(.headline)
+                if !phaseTimer.didEndPhase {
+                    Text(phaseTimer.formattedTime)
+                        .font(.headline)
+                }
                 
                 Text(phaseTimer.title)
                     .font(.headline)
@@ -23,12 +25,14 @@ struct MainListRow: View {
             }
             Spacer()
             
-            Button(action: onPlayButtonTap) {
-                Image(systemName: phaseTimer.isRunning ? "pause.circle.fill" : "play.circle.fill")
-                    .font(.system(size: 44))
-                    .foregroundColor(phaseTimer.isRunning ? .orange : .green)
+            if !phaseTimer.didEndPhase {
+                Button(action: onPlayButtonTap) {
+                    Image(systemName: phaseTimer.isRunning ? "pause.circle.fill" : "play.circle.fill")
+                        .font(.system(size: 44))
+                        .foregroundColor(phaseTimer.isRunning ? .orange : .green)
+                }
+                .buttonStyle(PlainButtonStyle())
             }
-            .buttonStyle(PlainButtonStyle())
         }
     }
 }
