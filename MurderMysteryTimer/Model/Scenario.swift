ada799fd10571bf1c9ba40dbf4726d3984fa5d52
Kazumi Hayashida <axhfaro@gmail.com>
2025-12-17 22:38:20 +0900

    samplePhaseを修正した

--- MurderMysteryTimer/Model/Scenario.swift
+++ MurderMysteryTimer/Model/Scenario.swift
@@ -12,13 +12,12 @@ struct Scenario: Identifiable, Codable, Equatable {
     
     var samplePhases: [ScenarioPhase] {
         return [ScenarioPhase(id: 1, title: "導入", seconds: 500),
-                ScenarioPhase(id: 1, title: "自己紹介", seconds: 500),
-                ScenarioPhase(id: 1, title: "第1調査フェーズ", seconds: 25*60),
-                ScenarioPhase(id: 1, title: "第1全体会議フェーズ", seconds: 10*60),
-                ScenarioPhase(id: 1, title: "第2調査フェーズ", seconds: 25*60),
-                ScenarioPhase(id: 1, title: "第3全体会議フェーズ", seconds: 10*60),
-                ScenarioPhase(id: 1, title: "最終弁論", seconds: 1*60),
-                ScenarioPhase(id: 1, title: "犯人拘束", seconds: 10*60)]
-
+                ScenarioPhase(id: 2, title: "自己紹介", seconds: 500),
+                ScenarioPhase(id: 3, title: "第1調査", seconds: 25*60),
+                ScenarioPhase(id: 4, title: "第1全体会議", seconds: 10*60),
+                ScenarioPhase(id: 5, title: "第2調査", seconds: 25*60),
+                ScenarioPhase(id: 6, title: "第2全体会議", seconds: 10*60),
+                ScenarioPhase(id: 7, title: "最終弁論", seconds: 1*60),
+                ScenarioPhase(id: 8, title: "犯人投票", seconds: 10*60)]
     }
 }
